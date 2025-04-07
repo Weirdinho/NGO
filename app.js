015cca37409697af929e704b21181888a334c19c
Umar Gusau <ruruwhygee@gmail.com>
2025-04-07 22:13:52 +0100

    modified:   app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, "frontend")));
 
 // Your existing routes
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', "frontend/home.html"));
+  res.sendFile(path.join(__dirname, "frontend/home.html"));
 });
 app.get("/about", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend/about.html"));
@@ -22,13 +22,13 @@ app.get("/gallery", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend/gallery.html"));
 });
 app.get("/project", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/project.html"));
+  res.sendFile(path.join(__dirname, "frontend/project.html")); 
 });
 app.get("/what-on", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend/what-on.html"));
 });
 
-const PORT = 5000;
+const PORT = 7000;
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
